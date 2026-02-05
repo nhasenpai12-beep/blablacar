fa669f18b29093d46f0e30a51836c475b420c9cf
nhasenpai12-beep <nhasenpai12@gmail.com>
2026-02-05 16:01:45 +0700

    actually saving the code now

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -5,24 +5,25 @@ import '../model/ride/ride.dart';
 class RidesService {
   static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
-  //
-  //  filter the rides starting from given departure location
-  //
   static List<Ride> _filterByDeparture(Location departure) {
-    return [];
+    return availableRides
+        .where((availableRides) => availableRides.arrivalLocation == departure)
+        .toList();
   }
 
-  //
-  //  filter the rides starting for the given requested seat number
-  //
   static List<Ride> _filterBySeatRequested(int requestedSeat) {
-    return [];
+    return availableRides
+        .where(
+          (availableRides) => availableRides.availableSeats == requestedSeat,
+        )
+        .toList();
   }
 
-  //
-  //  filter the rides   with several optional criteria (flexible filter options)
-  //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return [];
+    return availableRides
+        .where(
+          (availableRides) => availableRides.availableSeats == seatRequested,
+        )
+        .toList();
   }
 }
